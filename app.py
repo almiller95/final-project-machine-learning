8e4946c2266721db111f77c122dd6c2fedf789ae
Collin Page <collinmpage@gmail.com>
2021-06-09 19:06:08 -0500

    uploading all edits

--- app.py
+++ app.py
@@ -17,45 +17,67 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///world_happiness_all.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cancer_data.sqlite"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-#################################################
+# ################################################
 # Create class to get data
-#################################################
-# class Data(db.Model):
-#     __tablename__ = 'data_all'
-    
-#     id = db.Column(db.Integer, primary_key = True)
-#     overall_rank = db.Column(db.String(50))
-#     country_or_region = db.Column(db.String(50))
-#     happiness_score = db.Column(db.Numeric(2,2))
-#     gdp_per_capita = db.Column(db.Numeric(2,2))
-#     social_support = db.Column(db.Numeric(2,2))
-#     life_expectancy = db.Column(db.Numeric(2,2))
-#     freedom = db.Column(db.Numeric(2,2))
-#     generosity = db.Column(db.Numeric(2,2))
-#     perception_of_government_corruption = db.Column(db.Numeric(2,2))
-#     year = db.Column(db.String(4))
+# ################################################
+class Data(db.Model):
+    __tablename__ = 'cancer_data'
+        
+    id = db.Column(db.Integer, primary_key = True)
+    diagnosis = db.Column(db.String(50))
+    radius_mean = db.Column(db.Numeric(2,6))
+    texture_mean = db.Column(db.Numeric(2,6))
+    perimeter_mean = db.Column(db.Numeric(2,6))
+    area_mean = db.Column(db.Numeric(2,6))
+    smoothness_mean = db.Column(db.Numeric(2,6))
+    compactness_mean = db.Column(db.Numeric(2,6))
+    concavity_mean = db.Column(db.Numeric(2,6))
+    concave_points_mean = db.Column(db.Numeric(2,6))
+    symmetry_mean = db.Column(db.Numeric(2,6))
+    fractal_dimension_mean = db.Column(db.Numeric(2,6))
+    radius_se = db.Column(db.Numeric(2,6))
+    texture_se = db.Column(db.Numeric(2,6))
+    perimeter_se = db.Column(db.Numeric(2,6))
+    area_se = db.Column(db.Numeric(2,6))
+    smoothness_se = db.Column(db.Numeric(2,6))
+    compactness_se = db.Column(db.Numeric(2,6))
+    concavity_se = db.Column(db.Numeric(2,6))
+    concave_points_se = db.Column(db.Numeric(2,6))
+    symmetry_se = db.Column(db.Numeric(2,6))
+    fractal_dimension_se = db.Column(db.Numeric(2,6))
+    radius_worst = db.Column(db.Numeric(2,6))
+    texture_worst = db.Column(db.Numeric(2,6))
+    perimeter_worst = db.Column(db.Numeric(2,6))
+    area_worst = db.Column(db.Numeric(2,6))
+    smoothness_worst = db.Column(db.Numeric(2,6))
+    compactness_worst = db.Column(db.Numeric(2,6))
+    concavity_worst = db.Column(db.Numeric(2,6))
+    concave_points_worst = db.Column(db.Numeric(2,6))
+    symmetry_worst = db.Column(db.Numeric(2,6))
+    fractal_dimension_worst = db.Column(db.Numeric(2,6))
+
 
 
 # #################################################
 # # Create data page w/ sqlite connection
 # #################################################
 
-# @app.route("/data")
-# def data():
+@app.route("/data")
+def data():
 
-#     results = db.session.query(Data.overall_rank, Data.country_or_region, Data.happiness_score, 
-#     Data.gdp_per_capita, Data.social_support, Data.life_expectancy, Data.freedom, Data.generosity, 
-#     Data.perception_of_government_corruption, Data.year).all()
+    results = db.session.query(Data.overall_rank, Data.country_or_region, Data.happiness_score, 
+    Data.gdp_per_capita, Data.social_support, Data.life_expectancy, Data.freedom, Data.generosity, 
+    Data.perception_of_government_corruption, Data.year).all()
 
-#     print(results[0])
+    print(results[0])
 
-#     return render_template('data.html', data = results)
+    return render_template('data.html', data = results)
 
 
 
